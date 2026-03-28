7cf575fc77155c1811c3ea78a4b75e808bf69744
Rohit Bendre <rohitbendre27@gmail.com>
2026-03-28 16:36:16 +0530

    connected frontend to live backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000", // backend root
+  baseURL: "https://emotion-music-app-production.up.railway.app/analyze", // backend root
 });
 
 export async function analyzeEmotion(text) {
